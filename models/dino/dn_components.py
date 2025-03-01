3e96dfaea7376d93ec7a2efb292dc178406bc37a
Zhijing Xin <zhijingxin@stu.cqu.edu.cn>
2025-03-01 11:46:54 +0800

    去噪部分还差attn_mask

--- models/dino/dn_components.py
+++ models/dino/dn_components.py
@@ -18,7 +18,7 @@ import torch.nn.functional as F
 
 
 def prepare_for_cdn(dn_args, training, num_queries, num_classes, hidden_dim, label_enc):
-    """
+    """ 为对比去噪生成带噪声的查询，包括标签和边界框的噪声， 生成注意力掩码，控制去噪查询之间的可见性，避免信息泄露
         A major difference of DINO from DN-DETR is that the author process pattern embedding pattern embedding in its detector
         forward function and use learnable tgt embedding, so we change this function a little bit.
         :param dn_args: targets, dn_number, label_noise_ratio, box_noise_scale
@@ -26,19 +26,19 @@ def prepare_for_cdn(dn_args, training, num_queries, num_classes, hidden_dim, lab
         :param num_queries: number of queires
         :param num_classes: number of classes
         :param hidden_dim: transformer hidden dim
-        :param label_enc: encode labels in dn
+        :param label_enc: encode labels in dn 标签编码器，用于将标签映射到嵌入空间
         :return:
         """
     if training:
         targets, dn_number, label_noise_ratio, box_noise_scale = dn_args
         # positive and negative dn queries
         dn_number = dn_number * 2
-        known = [(torch.ones_like(t['labels'])).cuda() for t in targets]
+        known = [(torch.ones_like(t['labels'])).cuda() for t in targets] # 标记每个目标是否真实存在
         batch_size = len(known)
-        known_num = [sum(k) for k in known]
+        known_num = [sum(k) for k in known] # 每张照片的真实目标数
         if int(max(known_num)) == 0: # 没有物体的话，dn=1
             dn_number = 1
-        else:
+        else: # 若真实目标数过多，减少去噪组数，防止显存溢出
             if dn_number >= 100: # 如果dn_number > 100，为了防止使用过多资源，要适当降低资源实用度
                 dn_number = dn_number // (int(max(known_num) * 2)) # 为什么？
             elif dn_number < 1:
@@ -51,10 +51,10 @@ def prepare_for_cdn(dn_args, training, num_queries, num_classes, hidden_dim, lab
         batch_idx = torch.cat([torch.full_like(t['labels'].long(), i) for i, t in enumerate(targets)]) # 生成batch_id，即每个label,bbox在batch中的位置
 
         known_indice = torch.nonzero(unmask_label + unmask_bbox) # 返回输入tensor中非0元素的位置
-        known_indice = known_indice.view(-1)
+        known_indice = known_indice.view(-1) # 展平
         # 在debug的第一个batch里面，batch_size=2, 第一张图片有10个框，第二张图片有9个框，一组有19个元素，共20组
         known_indice = known_indice.repeat(2 * dn_number, 1).view(-1) # 2 * dn_number组
-        known_labels = labels.repeat(2 * dn_number, 1).view(-1)
+        known_labels = labels.repeat(2 * dn_number, 1).view(-1) # 展平
         known_bid = batch_idx.repeat(2 * dn_number, 1).view(-1)
         known_bboxs = boxes.repeat(2 * dn_number, 1)
         known_labels_expaned = known_labels.clone()
@@ -62,37 +62,42 @@ def prepare_for_cdn(dn_args, training, num_queries, num_classes, hidden_dim, lab
 
         if label_noise_ratio > 0:
             p = torch.rand_like(known_labels_expaned.float())
+            # 在label_noise_ratio=0.5的情况下，选出了1/4的总数，也就是负例中有一半的标签被随机替换
             chosen_indice = torch.nonzero(p < (label_noise_ratio * 0.5)).view(-1)  # half of bbox prob
             new_label = torch.randint_like(chosen_indice, 0, num_classes)  # randomly put a new one here
             known_labels_expaned.scatter_(0, chosen_indice, new_label)
-        single_pad = int(max(known_num))
+        single_pad = int(max(known_num)) # 这个batch中一张图片中最多有10个目标，所以都要填充成10，保证输入维度一致
 
         pad_size = int(single_pad * 2 * dn_number)
         positive_idx = torch.tensor(range(len(boxes))).long().cuda().unsqueeze(0).repeat(dn_number, 1)
-        positive_idx += (torch.tensor(range(dn_number)) * len(boxes) * 2).long().cuda().unsqueeze(1)
+        positive_idx += (torch.tensor(range(dn_number)) * len(boxes) * 2).long().cuda().unsqueeze(1) # 广播机制，算出来各组的起始编号然后相加
         positive_idx = positive_idx.flatten()
         negative_idx = positive_idx + len(boxes)
         if box_noise_scale > 0:
+            # known_bbox格式为 x_center, y_center, w, h的格式
             known_bbox_ = torch.zeros_like(known_bboxs)
             known_bbox_[:, :2] = known_bboxs[:, :2] - known_bboxs[:, 2:] / 2
-            known_bbox_[:, 2:] = known_bboxs[:, :2] + known_bboxs[:, 2:] / 2
+            known_bbox_[:, 2:] = known_bboxs[:, :2] + known_bboxs[:, 2:] / 2 # 转换为x_min, y_min, x_max, y_max格式
 
             diff = torch.zeros_like(known_bboxs)
-            diff[:, :2] = known_bboxs[:, 2:] / 2
-            diff[:, 2:] = known_bboxs[:, 2:] / 2
-
+            diff[:, :2] = known_bboxs[:, 2:] / 2 # 前两列是w/2, h/2
+            diff[:, 2:] = known_bboxs[:, 2:] / 2 # 后两列是w/2, h/2
+            # diff :[w/2, h/2, w/2, h/2]
             rand_sign = torch.randint_like(known_bboxs, low=0, high=2, dtype=torch.float32) * 2.0 - 1.0
             rand_part = torch.rand_like(known_bboxs)
-            rand_part[negative_idx] += 1.0
-            rand_part *= rand_sign
+            rand_part[negative_idx] += 1.0 # 正样本轻微扰动，负样本＋1，剧烈扰动
+            rand_part *= rand_sign # rand_part是对于扰动的程度大小
+            # 把坐标框转成xyxy格式，然后对框进行扰动，对负样本+1，扰动程度更大，rand_part是扰动程度的体现，后续*diff(w/2, h/2, w/2, h/2)
+            # 然后+ known_bbox即可以实现对框的扰动
             known_bbox_ = known_bbox_ + torch.mul(rand_part,
                                                   diff).cuda() * box_noise_scale
             known_bbox_ = known_bbox_.clamp(min=0.0, max=1.0)
-            known_bbox_expand[:, :2] = (known_bbox_[:, :2] + known_bbox_[:, 2:]) / 2
-            known_bbox_expand[:, 2:] = known_bbox_[:, 2:] - known_bbox_[:, :2]
+            known_bbox_expand[:, :2] = (known_bbox_[:, :2] + known_bbox_[:, 2:]) / 2 # x_center, y_center
+            known_bbox_expand[:, 2:] = known_bbox_[:, 2:] - known_bbox_[:, :2] # width, height
 
         m = known_labels_expaned.long().to('cuda')
-        input_label_embed = label_enc(m)
+        input_label_embed = label_enc(m) # label_queries
+        # 对数几率的形式，可以将值扩展到更广的范围，从而缓解梯度问题，使训练更加稳定。
         input_bbox_embed = inverse_sigmoid(known_bbox_expand)
 
         padding_label = torch.zeros(pad_size, hidden_dim).cuda()
@@ -102,10 +107,10 @@ def prepare_for_cdn(dn_args, training, num_queries, num_classes, hidden_dim, lab
         input_query_bbox = padding_bbox.repeat(batch_size, 1, 1)
 
         map_known_indice = torch.tensor([]).to('cuda')
-        if len(known_num):
+        if len(known_num): # 不明白
             map_known_indice = torch.cat([torch.tensor(range(num)) for num in known_num])  # [1,2, 1,2,3]
-            map_known_indice = torch.cat([map_known_indice + single_pad * i for i in range(2 * dn_number)]).long()
-        if len(known_bid):
+            map_known_indice = torch.cat([map_known_indice + single_pad * i for i in range(2 * dn_number)]).long() # 为每个去噪组（dn_group）添加偏移量
+        if len(known_bid): # 将标签嵌入input_label_embed和边界框嵌入input_bbox_embed填充到模型的输入张量中
             input_query_label[(known_bid.long(), map_known_indice)] = input_label_embed
             input_query_bbox[(known_bid.long(), map_known_indice)] = input_bbox_embed
 
